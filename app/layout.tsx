2644f4cb42a4c2431ed151200ed59979a87b04b4
shunyingzhang <>
2024-08-08 14:20:00 +1000

    Nav bar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Providers } from "./providers";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import NavBar from "../components/NavBar";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          <NavBar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
