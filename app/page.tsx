a0ab59629a42b5287277923d7ccf211270a26488
shunyingzhang <>
2024-08-09 10:34:54 +1000

    Get start robot button added on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,38 @@
-import { Image } from "@chakra-ui/react";
+'use client';
+import { Box, Image } from "@chakra-ui/react";
+import CustomButton from "@/components/CustomButton";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const handleButtonClick = () => {
+    router.push(`/robot`);
+  };
+
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <Image
-      alt={"product image"}
-      src="/backgroundImg.jpg"
-      fit="cover"
-      align="center"
+    <Box
+      position="relative"
       w="100%"
       h={{ base: "100%", sm: "400px", lg: "600px" }}
-    />
-    // </main>
+    >
+      <Image
+        alt="product image"
+        src="/backgroundImg.jpg"
+        objectFit="cover"
+        w="100%"
+        h="100%"
+      />
+      <CustomButton
+        position="absolute"
+        bottom="5%"
+        left="50%"
+        transform="translate(-50%, -50%)"
+        size="lg"
+        fontSize="2xl"
+        onClick={handleButtonClick}
+      >
+        Get Started the Robot
+      </CustomButton>
+    </Box>
   );
 }
